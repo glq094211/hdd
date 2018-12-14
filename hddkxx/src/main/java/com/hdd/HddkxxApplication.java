425f2e1a56b3464cf8f2f251ffec1b46df5f25a6
glq094211 <glq094211@163.com>
2018-12-14 18:06:03 +0800

    Update HddkxxApplication.java

--- hddkxx/src/main/java/com/hdd/HddkxxApplication.java
+++ hddkxx/src/main/java/com/hdd/HddkxxApplication.java
@@ -2,14 +2,19 @@ package com.hdd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HddkxxApplication {
+public class HddkxxApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HddkxxApplication.class, args);
 	}
 
-
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(HddkxxApplication.class);
+	}
 }
 
