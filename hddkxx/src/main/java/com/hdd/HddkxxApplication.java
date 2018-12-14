29c658e032aa02b3632fa5bedb37e506f8f95b85
glq094211 <glq094211@163.com>
2018-12-14 15:19:45 +0800

    初始化导入

---
+++ hddkxx/src/main/java/com/hdd/HddkxxApplication.java
@@ -0,0 +1,15 @@
+package com.hdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HddkxxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HddkxxApplication.class, args);
+	}
+
+
+}
+
