c93bfa8c394ac386a5a3766638bbcd1794da1232
glq094211 <45865386+glq094211@users.noreply.github.com>
2019-03-19 20:34:49 +0800

    fis
    普通提交

---
+++ hddkxx/src/test/java/com/hdd/util/BaseTest.java
@@ -0,0 +1,31 @@
+package com.hdd.util;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class BaseTest {
+
+    /*public static void main(String[] args) {
+        Integer [] integers = {5,7,2,1,8};
+        Arrays.sort(integers, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+*//*                int code = -1;
+                if(o1>o2){
+                    code = 0;
+                }*//*
+                return Integer.compare(o1,o2);
+            }
+        });
+        for (Integer integer :
+                integers) {
+            System.out.println(integer);
+        }
+    }*/
+
+    public static void main(String[] args) {
+        Integer [] integers = {5,7,2,1,8};
+        Arrays.sort(integers,(o1,o2) -> Integer.compare(o2,o1));
+        Arrays.stream(integers).forEach(System.out::println);
+    }
+}
